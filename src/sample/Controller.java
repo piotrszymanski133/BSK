587a6c4dafdb9d7b9f154283d1733e68720a630d
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2021-03-17 19:39:14 +0100

    added simple view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    ChoiceBox<String> modeChoiceBox;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<String> availableChoices = FXCollections.observableArrayList("ECB", "CBC", "CTR", "OFB", "CFB");
+        modeChoiceBox.setItems(availableChoices);
+        modeChoiceBox.setValue("ECB");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,22 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root,343.0, 350.0));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
